27020bc93f5508ea53d2f09c029be887d1cc1d35
Hyewon Im <hyewon.im@icloud.com>
2025-03-03 13:55:24 +0000

    Done initial setting

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<!-- +layout.svelte -->
+<script>
+	import Header from '$lib/components/Header.svelte';
+    import '$lib/styles/style.css';
+</script>
+
+<Header />
+<header>Hi, I'm a header</header>
+
+<main>
+	<slot />
+</main>
+
+<footer>Hello, I'm the footer.</footer>
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,4 @@
+<!-- contact/+page.svelte -->
+<h1>Get in touch</h1>
+
+<p><a href="hyewon.im@icloud.com">Email me!</a></p>
