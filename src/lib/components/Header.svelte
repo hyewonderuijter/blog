51f20290bd6ffddc8a0c7f6336c76f3fa0e4ce8c
Hyewon Im <hyewon.im@icloud.com>
2025-03-04 15:38:27 +0000

    Enhance header interactivity with hover effects; update profile image and add social icons

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,23 +1,48 @@
-<!-- Header.svelte -->
-<header>
-	<a href="/"><img src="duck.png" alt="profile of Hyewon" style="width:100px; transform:scaleX(-1)"></a>
+<script>
+	import { fade, scale } from 'svelte/transition';
+	let mouseHovered = false;
+	</script>
+	
+	<header>
+		<a href="/" 
+			on:mouseenter={() => mouseHovered = true} 
+			on:mouseleave={() => mouseHovered = false}
+		>
+			{#if mouseHovered}
+				<img 
+					width="70" 
+					src="https://img.icons8.com/3d-fluency/94/flying-duck.png" 
+					alt="flying-duck" 
+					in:scale={{ duration: 1000, start: 0.8 }}
+					class="logo stand"
+				/>
+			{:else}
+				<img 
+					width="70" 
+					src="https://img.icons8.com/3d-fluency/94/duck.png" 
+					alt="duck" 
+					in:scale={{ duration: 1500, start: 0.8 }}
+					class="logo flying"
 
-	<nav class="poppins-light">
-		<ul>
-			<li>
-				<a href="/about">About Me</a>
-			</li>
-			<li>
-				<a href="/blog">Blog</a>
-			</li>
-			<li>
-				<a href="/contact">Contact</a>
-			</li>
-		</ul>
-	</nav>
-</header>
-
-<style>
+				/>
+			{/if}
+		</a>
+		<nav class="poppins-light">
+			<ul>
+				<li>
+					<a href="/about">About Me</a>
+				</li>
+				<li>
+					<a href="/blog">Blog</a>
+				</li>
+				<li>
+					<a href="/contact">Contact</a>
+				</li>
+			</ul>
+		</nav>
+	</header>
+	
+	<style>
 	header {
 		padding: 1rem;
 		background: transparent;
@@ -25,27 +50,28 @@
 		flex-wrap: wrap;
 		justify-content: space-between;
 		align-items: center;
+		margin-top: .5rem;
 	}
 
+	.logo {
+		margin-left: 2rem;
+	}
 	ul {
 		margin: 0;
 		list-style-type: none;
 		display: flex;
 		gap: 3rem;
-		color: #757575;
+		color: rgb(255, 255, 255);
 	}
-
 	li:focus, li:hover {
 		color: #000;
 		transition: color 0.5s ease;
 	}
-
 	ul:last-child {
 		margin-right: 2rem;
 	}
-
 	a {
 		text-decoration: none;
 		color: inherit;
 	}
-</style>
+	</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,13 +13,18 @@
 <main>
     <div class="banner container poppins-regular">
             <div data-aos="fade-right" data-aos-duration="1300">
-                <img src="memoji.png" alt="profile of Hyewon">
+                <img src="memoji.png" alt="profile of Hyewon" class="main-profile-img">
             </div>
             <div data-aos="fade-left" data-aos-duration="1300">
                 <div class="textarea">
                     <h1>Hello, I'm Hyewon!</h1>
-                    <p>I'm a creative developer based in the Netherlands. I create, showcase remarkable design discoveries, and explore the continuous innovation that drives web development forward. </p>
-                    <p>The web is a constantly evolving platform, and that deeply resonates with me. This blog is a space where I share insights, lessons learned, and how I achieve my goals in my development journey.</p>
+                    <p class="first-para">I'm a <strong>creative developer</strong> based in the Netherlands 🇳🇱<br/> I create, showcase remarkable design discoveries, and explore the continuous innovation that drives web development forward. </p>
+                    <p class="second-para">The web is a constantly evolving platform, and that deeply resonates with me. This blog is a space where I share insights, lessons learned, and how I achieve my goals in my development journey.</p>
+                </div>
+                <div class="icon-list">
+                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/github.png" alt="github" class="icon"/>
+                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/domain.png" alt="domain" class="icon"/>
+                    <img width="30" height="30" src="https://img.icons8.com/ios-glyphs/30/linkedin.png" alt="linkedin" class="icon"/>
                 </div>
             </div>
     </div>
@@ -27,6 +32,7 @@
 
 <style>
     main {
+            margin-top: 6rem;
             background-color: rgba(255, 255, 255, 0.2);
             width: 100vw;
         }
@@ -45,16 +51,28 @@
         gap: 100px;
     }
 
-    img {
+    .main-profile-img {
         width: 450px;
     }
 
     h1 {
         font-size: 3rem;
+        margin-bottom: 0;
     }
 
     p {
         font-size: 1.1rem;
+    }
+
+    .icon-list {
+        display: flex;
+        flex-direction: row;
+        gap: 20px;
+    }
+
+    .icon:focus, .icon:hover {
+        transform: scale(1.2);
+        transition: transform 0.5s ease;
     }
 
 </style>
